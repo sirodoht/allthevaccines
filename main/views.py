442303d306ba856d6652b0ea722a71ead805c92a
Theodore Keloglou <zf@sirodoht.com>
2021-12-20 03:07:38 +0000

    add index template listview

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from main import models
 
 
-def index(request):
-    return render(request, "main/index.html")
+class IndexView(ListView):
+    model = models.Vaccine
+    template_name = "main/index.html"
