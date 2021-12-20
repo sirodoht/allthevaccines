aaa01c9fb21d3d738ff91b3dca9d609b7576ebfc
Theodore Keloglou <zf@sirodoht.com>
2021-12-20 02:32:41 +0000

    add vaccine and disease models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+class VaccineAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "trade_name",
+        "manufacturer",
+    )
+    list_display_links = ("id", "trade_name")
+
+    ordering = ["-id"]
+
+
+admin.site.register(models.Vaccine, VaccineAdmin)
+
+
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    list_display_links = ("id", "name")
+
+    ordering = ["-id"]
+
+
+admin.site.register(models.Disease, DiseaseAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Vaccine(models.Model):
+    trade_name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["trade_name"]
+
+    def __str__(self):
+        return self.trade_name
+
+
+class Disease(models.Model):
+    name = models.CharField(max_length=100)
+    wikipedia_url = models.CharField(max_length=100)
+    vaccines = models.ManyToManyField(Vaccine)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
