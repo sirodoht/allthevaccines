e2774f1fbdf84b2d4039973e2eb7b1b36576c29e
Theodore Keloglou <zf@sirodoht.com>
2021-12-21 16:09:34 +0000

    add vaccine pages

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.Index.as_view(), name="index"),  # vaccine_list
+    path("vaccine/<slug:slug>/", views.VaccineDetail.as_view(), name="vaccine_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from main import models
 
 
-class IndexView(ListView):
+class Index(ListView):
     model = models.Vaccine
     template_name = "main/index.html"
+
+class VaccineDetail(DetailView):
+    model = models.Vaccine
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["disease_list"] = models.Disease.objects.filter(vaccines=self.object)
+        return context
