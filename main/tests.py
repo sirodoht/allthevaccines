5d6ba5458846921d930e712def05f3761a35cded
Theodore Keloglou <zf@sirodoht.com>
2021-12-31 00:19:22 +0200

    add test for about static page

--- main/tests.py
+++ main/tests.py
@@ -7,3 +7,10 @@ class IndexTestCase(TestCase):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "allthevaccines")
+
+
+class AboutTestCase(TestCase):
+    def test_about(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "About")
