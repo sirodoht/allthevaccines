6883004fc8ce3a31cd516a04331f4368bbc04667
Theodore Keloglou <zf@sirodoht.com>
2022-01-01 06:00:44 +0200

    add test for subscribe form

--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
+
+from main import models
 
 
 class IndexTestCase(TestCase):
@@ -14,3 +18,36 @@ class AboutTestCase(TestCase):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "About")
+
+
+class SubscriptionTestCase(TestCase):
+    def test_subscribe_post(self):
+        response = self.client.post(
+            reverse("subscribe"),
+            {
+                "email": "tester@example.com",
+            },
+            follow=True,
+        )
+
+        # verify request
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Thanks! Email saved.")
+
+        # verify model
+        self.assertEqual(models.Subscription.objects.all().count(), 1)
+        self.assertEqual(
+            models.Subscription.objects.all()[0].email, "tester@example.com"
+        )
+
+        # verify email message
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn("New subscription: tester@example.com", mail.outbox[0].subject)
+        self.assertIn("tester@example.com", mail.outbox[0].body)
+
+        # verify email headers
+        self.assertEqual(mail.outbox[0].to, [settings.ADMINS[0][1]])
+        self.assertEqual(
+            mail.outbox[0].from_email,
+            settings.SERVER_EMAIL,
+        )
