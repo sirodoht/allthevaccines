6d298a91e88fcdb7273042a695400b8ccfc3f91e
Theodore Keloglou <zf@sirodoht.com>
2025-12-15 22:49:02 +0000

    change debug setting

--- allthevaccines/settings.py
+++ allthevaccines/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "nonrandom_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get("DEBUG") == "1" else False
+DEBUG = os.getenv("DEBUG") == "1"
 
 ALLOWED_HOSTS = [
     "localhost",
